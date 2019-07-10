42354657893219b04e7cabcc99a140d1fea55112
BulgakViktoriya14 <bulgak.viktoriya@gmail.com>
2019-07-10 12:55:24 +0300

    correct errors in the file

--- task3/script.ts
+++ task3/script.ts
@@ -2,15 +2,15 @@ import { parse } from 'papaparse';
 import { readFileSync, writeFileSync } from 'fs';
 import { config } from  './config';
 
-let users: object[] = [],
-indexRemove: number = 0,
-countRemove: number = 1,
-validData: object[] = [],
-invalidData: object[] = [];
+let users: object[] = [];
+let validData: object[] = [];
+let invalidData: object[] = [];
+const INDEX_REMOVE: number = 0;
+const COUNT_REMOVE: number = 1;
 
 function parseData(file) {
 	parse(file, {complete: (result) => {
-		result.data.splice(indexRemove, countRemove);
+		result.data.splice(INDEX_REMOVE, COUNT_REMOVE);
 		result.data.forEach((user: object) => {
 			users.push({
 				ID: user[0],
@@ -26,13 +26,13 @@ function parseData(file) {
 }
 
 function startValidate(users : object[], config : object[]) : void {
-	const file = readFileSync('./data/Users.csv', 'utf8');
-	parseData(file);
+	const FILE = readFileSync('./data/Users.csv', 'utf8');
+	parseData(FILE);
 	users.forEach((user: object) => { 
 		let testResults : boolean[] = [];
 		for(let key in user) {
 			config.forEach((obj:object) => {
-				if(key == obj['name']) {
+				if(key === obj['name']) {
 					testResults.push(...validateData(user[key],obj['validators'],key));
 				}
 			})
@@ -66,83 +66,74 @@ function validateData(data : string, rules: object, key: string) : Array<boolean
 	return arrayChecks;
 }
 
-function checkLength(len : string, data : string) : boolean {
-	let arrLen: string[] = len.split(','), 
-	startLen: number = Number(arrLen[0]),
-	endLen: number = Number(arrLen[1]),
-	dataLen: number = data.length;
-	if(dataLen >= startLen && dataLen <= endLen) {
+function checkLength(length : string, data : string) : boolean {
+	let arrLength: string[] = length.split(','); 
+	let startLength: number = Number(arrLength[0]);
+	let endLength: number = Number(arrLength[1]);
+	let dataLength: number = data.length;
+	if(dataLength >= startLength && dataLength <= endLength) {
 		return true;
-	} else { 
-		console.log(`Error!The length of this field is not correct: ${data}`);
-		return false;
-	}
+	} 
+	console.log(`Error!The length of this field is not correct: ${data}`);
+	return false;
 }
 
 function checkRequire(data : string) : boolean {
 	if(data != '') {
 		return true;
-	} else {
-		console.log(`Error!This field must not be empty: ${data}`);
-		return false;
-	}
+	} 
+	console.log(`Error!This field must not be empty: ${data}`);
+	return false;
 }
 
 function checkTemplate(templ : string, data : string) : boolean {
 	if(new RegExp(templ).test(data)) {
 		return true;
-	} else {
-		console.log(`Error!This field does not match the pattern: ${data}`);
-		return false;
-	}
+	} 
+	console.log(`Error!This field does not match the pattern: ${data}`);
+	return false;
 }
 
 function checkUnique(data : string, k : string) {
-	let valuesKey: string[] = [], count : number = 0;
-	users.forEach(function(user) {
+	let valuesKey: string[] = [];
+	users.forEach(function(user : object) {
 		for(let key in user) {
-			if(key == k) {
+			if(key === k) {
 				valuesKey.push(user[key]);
 			}
 		}
 	})
-	valuesKey.forEach(function(value) {
-		if(data == value) {
-			count++;
-		}
+	let newValuesKey = valuesKey.filter(function(value : string) {
+		return value === data;
 	})
-	if(count == 1) {
+	if(newValuesKey.length === 1) {
 		return true;
-	} else {
-		console.log(`Error!This field is not unique: ${data}`);
-		return false;
 	}
+	console.log(`Error!This field is not unique: ${data}`);
+	return false;
 }
 
 function checkFirstLetter(data : string) {
-	if(data[0].toUpperCase() == data[0]) {
+	if(data[0].toUpperCase() === data[0]) {
 		return true;
-	} else {
-		console.log(`Error!This field begins with a small letter: ${data}`);
-		return false;
-	}
+	} 
+	console.log(`Error!This field begins with a small letter: ${data}`);
+	return false;
 }
 
-function writeDataInArray(results : boolean[],user : object) {
-	let len : number = results.length, k : number = 0;
-	results.forEach(function(res : boolean) {
-		if(res) {
-			k++;
-		}
+function writeDataInArray(results : boolean[],user : object) : void {
+	let length : number = results.length;
+	let resultsTrue = results.filter(function(res : boolean) {
+		return res === true;
 	})
-	if(k == len) {
+	if(resultsTrue.length === length) {
 		validData.push(user);
 	} else {
 		invalidData.push(user);
 	}
 }
 
-function writeDataInFile(array : object[], path: string) {
+function writeDataInFile(array : object[], path: string) : void {
 	writeFileSync(path, JSON.stringify(array, null) , 'utf-8');
 }
 
